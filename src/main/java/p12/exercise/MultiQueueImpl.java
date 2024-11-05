17d25549dcb35f172c92e72cd26cf83968a37dee
Francesco Meloni <francesco.meloni6@studio.unibo.it>
2024-11-05 18:15:44 +0100

    Changed ThrowIf methods in checkIf and removed all the unnecessary empty lines

--- src/main/java/p12/exercise/MultiQueueImpl.java
+++ src/main/java/p12/exercise/MultiQueueImpl.java
@@ -22,97 +22,84 @@ public class MultiQueueImpl<T, Q> implements MultiQueue<T, Q>{
 
     @Override
     public void openNewQueue(Q queue) {
-        ThrowIfQueueAvailable(queue);
-
+        checkIfQueueAvailable(queue);
         multiQueue.put(queue, new LinkedHashSet<>());
     }
 
     @Override
     public boolean isQueueEmpty(Q queue) {
-        ThrowIfQueueNotAvailable(queue);
-
+        checkIfQueueNotAvailable(queue);
         return multiQueue.get(queue).isEmpty();
     }
 
     @Override
     public void enqueue(T elem, Q queue) {
-        ThrowIfQueueNotAvailable(queue);
-
+        checkIfQueueNotAvailable(queue);
         multiQueue.get(queue).addLast(elem);
     }
 
     @Override
     public T dequeue(Q queue) {
-        ThrowIfQueueNotAvailable(queue);
-        
+        checkIfQueueNotAvailable(queue);
         LinkedHashSet<T> tmp = multiQueue.get(queue);
         if (tmp.isEmpty()) {
             return null;
         }
         T elem = tmp.removeFirst();
         multiQueue.put(queue, tmp);
-
         return elem;
     }
 
     @Override
     public Map<Q, T> dequeueOneFromAllQueues() {
         HashMap<Q, T> dequeuedEntries = new HashMap<>();
-
         for (Q queue : availableQueues()) {
             dequeuedEntries.put(queue, dequeue(queue)); 
         }
-
         return dequeuedEntries;
     }
 
     @Override
     public Set<T> allEnqueuedElements() {
         LinkedHashSet<T> enqueuedElements = new LinkedHashSet<>();
-        
         for (Q queue : availableQueues()) {
             enqueuedElements.addAll(multiQueue.get(queue));
         }
-
         return enqueuedElements;
     }
 
     @Override
     public List<T> dequeueAllFromQueue(Q queue) {
-        ThrowIfQueueNotAvailable(queue);
-
+        checkIfQueueNotAvailable(queue);
         List<T> dequeuedQueue = List.copyOf(multiQueue.get(queue));
         multiQueue.get(queue).clear();
-
         return dequeuedQueue;
     }
 
     @Override
     public void closeQueueAndReallocate(Q queue) {
-        ThrowIfQueueNotAvailable(queue);
-        ThrowIfSingleQueueRemaining();
-
+        checkIfQueueNotAvailable(queue);
+        checkIfSingleQueueRemaining();
         List<T> tmp = dequeueAllFromQueue(queue);
         multiQueue.remove(queue);
         multiQueue.get(availableQueues().iterator().next()).addAll(tmp);
     }
 
-    private void ThrowIfQueueAvailable(Q queue) {
+    private void checkIfQueueAvailable(Q queue) {
         if (multiQueue.containsKey(queue)) {
             throw new IllegalArgumentException("The queue " + queue + " is already available");
         }
     }
 
-    private void ThrowIfQueueNotAvailable(Q queue) {
+    private void checkIfQueueNotAvailable(Q queue) {
         if (!multiQueue.containsKey(queue)) { 
             throw new IllegalArgumentException("The queue " + queue + " does not exist");
         }
     }
 
-    private void ThrowIfSingleQueueRemaining() {
+    private void checkIfSingleQueueRemaining() {
         if (availableQueues().size() <= 1) {
             throw new IllegalStateException("There is no alternative queue for moving elements to");
         }
     }
-
 }
